e2c9c116686815430844c960eae370e4b6acd44a
Benoit Hediard <hediard@affinitiz.com>
2012-12-12 22:48:40 +0100

    Resource bug fix

--- grails-app/conf/SentryResources.groovy
+++ grails-app/conf/SentryResources.groovy
@@ -1,6 +1,6 @@
 modules = {
     'sentry-js' {
         dependsOn 'jquery'
-        resource url: [plugin: 'grails-sentry', dir: 'js', file: 'raven-0.6.min.js'], disposition: 'head'
+        resource url: [plugin: 'sentry', dir: 'js', file: 'raven-0.6.min.js'], disposition: 'head'
     }
 }
